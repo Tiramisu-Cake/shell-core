254a21c16c4910763616455fbdf6043933bda1cb
Tiramisu-Cake <igkharin@gmail.com>
2025-11-06 16:22:30 +0400

    3nd stage: REPL

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut cmd = String::new();
-    io::stdin().read_line(&mut cmd).unwrap();
-    println!("{}: command not found", cmd.trim());
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut cmd = String::new();
+        io::stdin().read_line(&mut cmd).unwrap();
+        println!("{}: command not found", cmd.trim());
+    }
 }

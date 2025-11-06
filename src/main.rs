0fd4e17c72b2234814c997b84255a250ec5c9413
Tiramisu-Cake <igkharin@gmail.com>
2025-11-06 16:18:53 +0400

    2nd stage: command not found

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut cmd = String::new();
+    io::stdin().read_line(&mut cmd).unwrap();
+    println!("{}: command not found", cmd);
 }

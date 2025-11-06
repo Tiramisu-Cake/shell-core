0f22ea58e36b224411b18643bcff8be5b8c43f8d
Tiramisu-Cake <igkharin@gmail.com>
2025-11-06 16:26:28 +0400

    4nd stage: exit 0

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut cmd = String::new();
         io::stdin().read_line(&mut cmd).unwrap();
+        if cmd == "exit 0" {
+            println!("Exit with status 0 (success)");
+            break;
+        }
         println!("{}: command not found", cmd.trim());
     }
 }

23cb4d4e80099217166765fadbdd243424e5ece5
Tiramisu-Cake <igkharin@gmail.com>
2025-11-19 13:06:09 +0400

    multiple pipeline

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ use libc::STDIN_FILENO;
 use libc::STDOUT_FILENO;
 use libc::{close, dup2};
 use nix::sys::wait::{waitpid, WaitStatus};
+use nix::unistd::Pid;
 use nix::unistd::{fork, pipe, ForkResult};
 use std::fs::{File, OpenOptions};
+use std::io::Error;
 use std::io::{self, Write};
 use std::os::fd::IntoRawFd;
 use std::os::unix::io::AsRawFd;
 use std::process::exit;
+use std::slice::Iter;
 
 fn open_truncate_file(path: &str) -> io::Result<File> {
     OpenOptions::new()
@@ -31,7 +34,7 @@ fn open_append_file(path: &str) -> io::Result<File> {
         .open(path)
 }
 
-fn open_and_get_fd(target_file: &TargetFile) -> File {
+fn open_file(target_file: &TargetFile) -> File {
     let file;
     if target_file.append {
         file = open_append_file(&target_file.path).expect("failed to open redirection file");
@@ -41,6 +44,21 @@ fn open_and_get_fd(target_file: &TargetFile) -> File {
     file
 }
 
+fn match_std(std: &StreamTarget, target_fd: i32) -> Result<Option<FdRedirectGuard>, Error> {
+    match std {
+        StreamTarget::Terminal => Ok(None),
+        StreamTarget::File(target_file) => {
+            let file = open_file(target_file);
+            let file_fd = file.as_raw_fd();
+            io::stdout().flush().ok();
+            match FdRedirectGuard::new(target_fd, file_fd) {
+                Ok(guard) => Ok(Some(guard)),
+                Err(e) => Err(e),
+            }
+        }
+    }
+}
+
 fn run_simplecmd(cmd: &SimpleCmd) {
     let args = &cmd.args;
     let stdout = &cmd.stdout;
@@ -48,28 +66,8 @@ fn run_simplecmd(cmd: &SimpleCmd) {
 
     let cmd = &args[0];
 
-    let _guard_stdout;
-    let _guard_stderr;
-
-    match stdout {
-        StreamTarget::Terminal => (),
-        StreamTarget::File(target_file) => {
-            let file = open_and_get_fd(target_file);
-            let file_fd = file.as_raw_fd();
-            io::stdout().flush().ok();
-            _guard_stdout = FdRedirectGuard::new(1, file_fd);
-        }
-    }
-
-    match stderr {
-        StreamTarget::Terminal => (),
-        StreamTarget::File(target_file) => {
-            let file = open_and_get_fd(target_file);
-            let file_fd = file.as_raw_fd();
-            io::stdout().flush().ok();
-            _guard_stderr = FdRedirectGuard::new(2, file_fd);
-        }
-    }
+    let _guard_stdout = match_std(stdout, 1);
+    let _guard_stderr = match_std(stderr, 2);
 
     match cmd.as_str() {
         "cd" => cd_cmd(&args[1..]),
@@ -99,34 +97,86 @@ fn main() {
             run_simplecmd(&config[0]);
             continue;
         }
-        let (read_end, write_end) = pipe().unwrap();
-        let write_fd = write_end.into_raw_fd();
-        let read_fd = read_end.into_raw_fd();
-        let fork1 = unsafe { fork() }.unwrap();
-        match fork1 {
-            ForkResult::Parent { child: pid1 } => {
-                unsafe { close(write_fd) };
-                let fork2 = unsafe { fork() }.unwrap();
-                match fork2 {
-                    ForkResult::Parent { child: pid2 } => {
-                        unsafe { close(read_fd) };
-                        let _status1 = waitpid(pid1, None);
-                        let _status2 = waitpid(pid2, None);
-                    }
-                    ForkResult::Child => {
-                        unsafe { dup2(read_fd, STDIN_FILENO) };
-                        unsafe { close(read_fd) };
-                        run_simplecmd(&config[1]);
-                        exit(0);
+
+        let mut it = config.iter();
+        let cmd1 = it.next().unwrap();
+        let cmd2 = it.next();
+        run_pipeline(&mut it, cmd1, cmd2, None, Vec::new());
+    }
+}
+
+fn run_pipeline(
+    mut cmds: &mut Iter<'_, SimpleCmd>,
+    cmd1: &SimpleCmd,
+    cmd2: Option<&SimpleCmd>,
+    read_fd: Option<i32>,
+    mut pids: Vec<Pid>,
+) {
+    match cmd2 {
+        None => {
+            match read_fd {
+                None => (),
+                Some(read_fd) => {
+                    let fork = unsafe { fork() }.unwrap();
+                    match fork {
+                        ForkResult::Parent { child: pid } => {
+                            pids.push(pid);
+                            unsafe { close(read_fd) };
+                            let mut waitpids = Vec::new();
+                            for pid in pids {
+                                waitpids.push(waitpid(pid, None));
+                            }
+                            // let _status = waitpid(pid, None);
+                        }
+                        ForkResult::Child => {
+                            unsafe {
+                                dup2(read_fd, STDIN_FILENO);
+                                close(read_fd);
+                            };
+                            run_simplecmd(cmd1);
+                            exit(0);
+                        }
                     }
                 }
             }
-            ForkResult::Child => {
-                unsafe { close(read_fd) };
-                unsafe { dup2(write_fd, STDOUT_FILENO) };
-                unsafe { close(write_fd) };
-                run_simplecmd(&config[0]);
-                exit(0);
+        }
+        Some(cmd) => {
+            let (read_end, write_end) = pipe().unwrap();
+            let write_fd = write_end.into_raw_fd();
+            let next_read_fd = read_end.into_raw_fd();
+
+            let next_fork = unsafe { fork() }.unwrap();
+            match next_fork {
+                ForkResult::Parent { child: pid } => {
+                    unsafe {
+                        match read_fd {
+                            None => (),
+                            Some(read_fd) => {
+                                close(read_fd);
+                            }
+                        }
+                        close(write_fd);
+                    };
+                    pids.push(pid);
+                    let next_cmd = cmds.next();
+                    run_pipeline(&mut cmds, cmd, next_cmd, Some(next_read_fd), pids);
+                }
+                ForkResult::Child => {
+                    unsafe {
+                        close(next_read_fd);
+                        match read_fd {
+                            None => (),
+                            Some(x) => {
+                                dup2(x, STDIN_FILENO);
+                                close(x);
+                            }
+                        }
+                        dup2(write_fd, STDOUT_FILENO);
+                        close(write_fd);
+                    };
+                    run_simplecmd(cmd1);
+                    exit(0);
+                }
             }
         }
     }
